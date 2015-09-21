c17ff2e78f6650200838a945c876e679605d037c
Matheus Braga Almeida <matheus.mtb7@gmail.com>
2015-09-21 16:36:08 -0300

    start stuff

---
+++ HelloWorldSwing.java
@@ -0,0 +1,44 @@
+
+/*
+ * HelloWorldSwing.java requires no other files.
+ */
+import javax.swing.*;
+
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelloWorldSwing {
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+        GroupLayout gl = new GroupLayout(frame.getContentPane());
+        frame.getContentPane().setLayout(gl);
+        frame.setSize(300, 200);
+
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
